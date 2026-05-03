f323da99e5fc6bbd58c5ae46441a3290ee5ab856
darknike23 <valdesantonio888@gmail.com>
2026-05-03 18:53:21 -0400

    Create styles.cs

---
+++ styles.cs
@@ -0,0 +1,23 @@
+body {
+  font-family: Arial;
+  background:#0f172a;
+  color:white;
+  text-align:center;
+}
+
+#chatbox {
+  height:300px;
+  background:black;
+  overflow:auto;
+  margin:20px;
+  padding:10px;
+}
+
+input {
+  padding:10px;
+  width:60%;
+}
+
+button {
+  padding:10px;
+}
